e00c3ef459971fa31f4600f1ece6d6849d5675be
fisherxu <xufei40@huawei.com>
2017-12-21 16:59:34 +0800

    delete networkpolicy in extension

--- staging/src/k8s.io/api/extensions/v1beta1/register.go
+++ staging/src/k8s.io/api/extensions/v1beta1/register.go
@@ -57,8 +57,6 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&ReplicaSetList{},
 		&PodSecurityPolicy{},
 		&PodSecurityPolicyList{},
-		&NetworkPolicy{},
-		&NetworkPolicyList{},
 	)
 	// Add the watch version that applies
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
