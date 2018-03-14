8a58ed52e5cab85338a93175d69305629dfc5723
fisherxu <xufei40@huawei.com>
2018-03-14 16:49:37 +0800

    kubectl

--- pkg/kubectl/rollout_status.go
+++ pkg/kubectl/rollout_status.go
@@ -19,12 +19,11 @@ package kubectl
 import (
 	"fmt"
 
-	extensionsv1beta1 "k8s.io/api/extensions/v1beta1"
+	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/kubernetes"
-	clientappsv1beta1 "k8s.io/client-go/kubernetes/typed/apps/v1beta1"
-	clientextensionsv1beta1 "k8s.io/client-go/kubernetes/typed/extensions/v1beta1"
+	clientappsv1 "k8s.io/client-go/kubernetes/typed/apps/v1"
 	"k8s.io/kubernetes/pkg/apis/apps"
 	"k8s.io/kubernetes/pkg/controller/deployment/util"
 )
@@ -37,29 +36,29 @@ type StatusViewer interface {
 // StatusViewerFor returns a StatusViewer for the resource specified by kind.
 func StatusViewerFor(kind schema.GroupKind, c kubernetes.Interface) (StatusViewer, error) {
 	switch kind {
-	case extensionsv1beta1.SchemeGroupVersion.WithKind("Deployment").GroupKind(), apps.Kind("Deployment"):
-		return &DeploymentStatusViewer{c.ExtensionsV1beta1()}, nil
-	case extensionsv1beta1.SchemeGroupVersion.WithKind("DaemonSet").GroupKind(), apps.Kind("DaemonSet"):
-		return &DaemonSetStatusViewer{c.ExtensionsV1beta1()}, nil
+	case appsv1.SchemeGroupVersion.WithKind("Deployment").GroupKind(), apps.Kind("Deployment"):
+		return &DeploymentStatusViewer{c.AppsV1()}, nil
+	case appsv1.SchemeGroupVersion.WithKind("DaemonSet").GroupKind(), apps.Kind("DaemonSet"):
+		return &DaemonSetStatusViewer{c.AppsV1()}, nil
 	case apps.Kind("StatefulSet"):
-		return &StatefulSetStatusViewer{c.AppsV1beta1()}, nil
+		return &StatefulSetStatusViewer{c.AppsV1()}, nil
 	}
 	return nil, fmt.Errorf("no status viewer has been implemented for %v", kind)
 }
 
 // DeploymentStatusViewer implements the StatusViewer interface.
 type DeploymentStatusViewer struct {
-	c clientextensionsv1beta1.DeploymentsGetter
+	c clientappsv1.DeploymentsGetter
 }
 
 // DaemonSetStatusViewer implements the StatusViewer interface.
 type DaemonSetStatusViewer struct {
-	c clientextensionsv1beta1.DaemonSetsGetter
+	c clientappsv1.DaemonSetsGetter
 }
 
 // StatefulSetStatusViewer implements the StatusViewer interface.
 type StatefulSetStatusViewer struct {
-	c clientappsv1beta1.StatefulSetsGetter
+	c clientappsv1.StatefulSetsGetter
 }
 
 // Status returns a message describing deployment status, and a bool value indicating if the status is considered done.
@@ -78,7 +77,7 @@ func (s *DeploymentStatusViewer) Status(namespace, name string, revision int64) 
 		}
 	}
 	if deployment.Generation <= deployment.Status.ObservedGeneration {
-		cond := util.GetDeploymentCondition(deployment.Status, extensionsv1beta1.DeploymentProgressing)
+		cond := util.GetDeploymentCondition(deployment.Status, appsv1.DeploymentProgressing)
 		if cond != nil && cond.Reason == util.TimedOutReason {
 			return "", false, fmt.Errorf("deployment %q exceeded its progress deadline", name)
 		}
@@ -104,7 +103,7 @@ func (s *DaemonSetStatusViewer) Status(namespace, name string, revision int64) (
 	if err != nil {
 		return "", false, err
 	}
-	if daemon.Spec.UpdateStrategy.Type != extensionsv1beta1.RollingUpdateDaemonSetStrategyType {
+	if daemon.Spec.UpdateStrategy.Type != appsv1.RollingUpdateDaemonSetStrategyType {
 		return "", true, fmt.Errorf("Status is available only for RollingUpdate strategy type")
 	}
 	if daemon.Generation <= daemon.Status.ObservedGeneration {
@@ -128,7 +127,7 @@ func (s *StatefulSetStatusViewer) Status(namespace, name string, revision int64)
 	if sts.Spec.UpdateStrategy.Type == apps.OnDeleteStatefulSetStrategyType {
 		return "", true, fmt.Errorf("%s updateStrategy does not have a Status`", apps.OnDeleteStatefulSetStrategyType)
 	}
-	if sts.Status.ObservedGeneration == nil || sts.Generation > *sts.Status.ObservedGeneration {
+	if sts.Generation > sts.Status.ObservedGeneration {
 		return "Waiting for statefulset spec update to be observed...\n", false, nil
 	}
 	if sts.Spec.Replicas != nil && sts.Status.ReadyReplicas < *sts.Spec.Replicas {
